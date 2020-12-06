bcb808dcf33524695ed05c5176024bf82d24b298
albertgoncalves <goncalves.albert@gmail.com>
2020-12-05 22:10:53 -0500

    random colors

--- src/vert.glsl
+++ src/vert.glsl
@@ -10,7 +10,12 @@ uniform float U_TIME;
 uniform mat4  U_PROJECTION;
 uniform mat4  U_VIEW;
 
+out vec3 VERT_OUT_COLOR;
+
 void main() {
+    VERT_OUT_COLOR.x = sin((U_TIME + IN_VERTEX.x) * 1.35);
+    VERT_OUT_COLOR.y = cos((U_TIME + IN_VERTEX.y) / 2.45);
+    VERT_OUT_COLOR.z = sin((U_TIME + IN_VERTEX.x) / 1.35);
     gl_Position =
         U_PROJECTION * U_VIEW * IN_TRANSLATE * U_MODEL * vec4(IN_VERTEX, 1.0);
 }
