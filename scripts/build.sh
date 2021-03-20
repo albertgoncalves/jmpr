7ec1311460397d12c0e3738bb91cb2943653e979
albertgoncalves <goncalves.albert@gmail.com>
2021-03-20 11:50:34 -0400

    clean-up build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -73,17 +73,12 @@ now () {
 
 (
     start=$(now)
-    for x in "$WD"/src/*.h; do
-        cppcheck "$x" | sed 's/\/.*\/\(.*\) \.\.\./\1/g'
-    done
     cppcheck \
         --enable=all \
         --suppress=missingIncludeSystem \
         "$WD/src" \
         | sed 's/\/.*\/\(.*\) \.\.\./\1/g'
-    clang-format -i -verbose "$WD/src"/*.glsl 2>&1 | sed 's/\/.*\///g'
-    clang-format -i -verbose "$WD/src"/*.c 2>&1 | sed 's/\/.*\///g'
-    clang-format -i -verbose "$WD/src"/*.h 2>&1 | sed 's/\/.*\///g'
+    clang-format -i -verbose "$WD/src"/* 2>&1 | sed 's/\/.*\///g'
     gcc \
         "${paths[@]}" \
         "${libs[@]}" \
